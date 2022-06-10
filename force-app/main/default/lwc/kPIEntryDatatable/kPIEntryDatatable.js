8ad7d54ce10490a7dadd215c3ebe02bf3005186c
stormymayday <stormymayday7@gmail.com>
2022-06-10 09:53:05 -0600

    Importing KPI_Entry_Controller into the kPIEntryDatatable

--- force-app/main/default/lwc/kPIEntryDatatable/kPIEntryDatatable.js
+++ force-app/main/default/lwc/kPIEntryDatatable/kPIEntryDatatable.js
@@ -1,3 +1,19 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, wire } from 'lwc';
+import get_KPI_Entries from '@salesforce/apex/KPI_Entry_Controller.get_KPI_Entries';
 
-export default class KPIEntryDatatable extends LightningElement {}
+export default class kpiEntryDatatable extends LightningElement {
+
+    @wire(get_KPI_Entries)
+    kpiEntryHandler({ data, error }) {
+
+        if (data) {
+            console.log(data);
+        }
+
+        if (error) {
+            console.error(error);
+        }
+
+    }
+
+}
