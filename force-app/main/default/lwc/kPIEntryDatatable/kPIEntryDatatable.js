f7667239af29e3d64528f1756b3c9b2a9538ade3
stormymayday <stormymayday7@gmail.com>
2022-06-10 10:07:36 -0600

    Rendering the Datatable if data is avaliable

--- force-app/main/default/lwc/kPIEntryDatatable/kPIEntryDatatable.js
+++ force-app/main/default/lwc/kPIEntryDatatable/kPIEntryDatatable.js
@@ -1,17 +1,35 @@
 import { LightningElement, wire } from 'lwc';
 import get_KPI_Entries from '@salesforce/apex/KPI_Entry_Controller.get_KPI_Entries';
 
+const COLUMNS = [
+    { label: 'Id', fieldName: 'Id' },
+    { label: 'Name', fieldName: 'Name' },
+    { label: 'Period Start Date', fieldName: 'Period_Start_Date__c' },
+    { label: 'Period End Date', fieldName: 'Date__c' },
+    { label: 'Value', fieldName: 'Value__c', editable: true },
+    { label: 'Value Type', fieldName: 'Value_Type__c' }
+]
+
 export default class kpiEntryDatatable extends LightningElement {
+
+    tableData
+    columns = COLUMNS
 
     @wire(get_KPI_Entries)
     kpiEntryHandler({ data, error }) {
 
         if (data) {
-            console.log(data);
+
+            // console.log(data);
+
+            this.tableData = data;
+
         }
 
         if (error) {
-            console.error(error);
+
+            // console.error(error);
+
         }
 
     }
